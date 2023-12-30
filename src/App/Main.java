a6f9acd820d137da4be2a4e6658d43aa9281cd40
shchelakov <shelakov92@gmail.com>
2023-12-30 02:33:29 +0100

    Add converting kilometers to miles

--- src/App/Main.java
+++ src/App/Main.java
@@ -1,15 +1,14 @@
 package App;
 
-
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     static String textInp;
     static double length;
-    static String lengthFormat;
+    static double cnvrtLength;
     static String[] unitOfMeasurement = {"km", "mile"};
-
+    private static final double COEFFICIENT = 1.609;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -19,22 +18,34 @@ public class Main {
         textInp = sc.nextLine().trim();
         int num = (int) Utils.checkNumber(textInp);
 
+        if (num >= 0 && num <= 1) {
 
-        System.out.println("Enter the length, " + unitOfMeasurement[num] + ":");
-        textInp = sc.nextLine().trim();
-        length = Utils.checkNumber(textInp);
-        lengthFormat = Utils.roundValue(length);
+            System.out.println("Enter the length, " + unitOfMeasurement[num] + ":");
+            textInp = sc.nextLine().trim();
+            length = Utils.checkNumber(textInp);
 
-        switch (num) {
-            case 0:
 
-                break;
-            case 1:
-                break;
-        }
+            switch (num) {
+                case 0:
+                    cnvrtLength = convertKmToMile(length);
 
+                    String lengthFormat = Utils.roundValue(length);
+                    String cnvrtLengthFormat = Utils.roundValue(cnvrtLength);
+
+                    System.out.printf("%s km = %s mile%n", lengthFormat, cnvrtLengthFormat);
+
+                    break;
+                case 1:
+                    break;
+            }
+
+        } else System.out.println(":(");
 
         sc.close();
-
     }
+    //------------------------------------------------------------------------------------------------------------------
+    public static double convertKmToMile(double length) {
+        return length / COEFFICIENT;
+    }
+    //------------------------------------------------------------------------------------------------------------------
 }
