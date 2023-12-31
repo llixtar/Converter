0ee741889ec15ab97cd433de7978afe32e3a5c9c
shchelakov <shelakov92@gmail.com>
2023-12-31 02:04:15 +0100

    Add fix

--- src/App/Main.java
+++ src/App/Main.java
@@ -9,39 +9,66 @@ public class Main {
     static double cnvrtLength;
     static String[] unitOfMeasurement = {"km", "mile"};
     private static final double COEFFICIENT = 1.609;
+    static Scanner sc;
 
+    //------------------------------------------------------------------------------------------------------------------
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        System.out.println("\nChoose: \n 0 - km \n 1 - mile");
+        run();
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void run() {
+        int num = chooseMeasurement();
+
+        switch (num) {
+            case 0: {
+                length = enterLength(num);
+                cnvrtLength = convertKmToMile(length);
+
+                String lengthFormat = Utils.roundValue(length);
+                String cnvrtLengthFormat = Utils.roundValue(cnvrtLength);
+
+                System.out.printf("%s km = %s mile%n", lengthFormat, cnvrtLengthFormat);
+                break;
+            }
+            case 1: {
+                length = enterLength(num);
+                break;
+            }
+            default:
+                System.out.println("¯\\_(ツ)_/¯");
+        }
+        quit();
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static void quit() {
+        System.out.println("\nEnter: \n 0 - quit \n 1 - continue");
         textInp = sc.nextLine().trim();
+
         int num = (int) Utils.checkNumber(textInp);
 
-        if (num >= 0 && num <= 1) {
-
-            System.out.println("Enter the length, " + unitOfMeasurement[num] + ":");
-            textInp = sc.nextLine().trim();
-            length = Utils.checkNumber(textInp);
-
-
-            switch (num) {
-                case 0:
-                    cnvrtLength = convertKmToMile(length);
-
-                    String lengthFormat = Utils.roundValue(length);
-                    String cnvrtLengthFormat = Utils.roundValue(cnvrtLength);
-
-                    System.out.printf("%s km = %s mile%n", lengthFormat, cnvrtLengthFormat);
-
-                    break;
-                case 1:
-                    break;
-            }
-
-        } else System.out.println(":(");
-
-        sc.close();
+        switch (num) {
+            case 0:
+                sc.close();
+                break;
+            case 1:
+                run();
+                break;
+        }
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static int chooseMeasurement() {
+        System.out.println("\nChoose: \n 0 - km \n 1 - mile");
+        textInp = sc.nextLine().trim();
+        return (int) Utils.checkNumber(textInp);
+    }
+    //------------------------------------------------------------------------------------------------------------------
+    public static double enterLength(int num) {
+        System.out.println("Enter the length, " + unitOfMeasurement[num] + ":");
+        textInp = sc.nextLine().trim();
+        return Utils.checkNumber(textInp);
     }
     //------------------------------------------------------------------------------------------------------------------
     public static double convertKmToMile(double length) {
